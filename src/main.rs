8f548530d95f0175daa8a10df54e7a97b7979505
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-02-20 12:00:49 -0400

    drop unnecesary functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::char::from_u32;
-
-use rand::{prelude::*, thread_rng, Rng, rngs::{adapter::ReseedingRng, OsRng}};
-use rand_chacha::{ChaCha8Rng, ChaCha20Core};
+use rand::{thread_rng, Rng};
 
 fn main() {
-    let password_length = 10;
+    let password_length = 12;
     let mut result = String::new();
 
     for _ in 0..password_length {
@@ -15,24 +13,4 @@ fn main() {
     }
 
     println!("{}", result);
-
-    generate_random_numbers_with_a_seed(1220);
-
-    non_deterministic();
-}
-
-fn generate_random_numbers_with_a_seed(seed: u64) {
-    let mut rng = ChaCha8Rng::seed_from_u64(seed);
-
-    println!(
-        "Deterministic Random numbers with seed:{}",
-        rng.gen::<i32>()
-    );
-}
-
-fn non_deterministic() {
-    let prng = ChaCha20Core::from_entropy();
-    let mut reseeding_rng = ReseedingRng::new(prng, 0, OsRng);
-
-    println!("Random number: {}", reseeding_rng.gen::<u64>());
 }
